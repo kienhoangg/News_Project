b6c64cad031ce57ce636243470c73e85aa053481
MinhUet <buivanminh1309@gmail.com>
2022-10-08 17:58:43 +0700

    feat: Bổ sung thông tin upload file

--- sources/client/news-ui/src/features/admin/pages/news/NewsFieldPage/NewsFieldPage.jsx
+++ sources/client/news-ui/src/features/admin/pages/news/NewsFieldPage/NewsFieldPage.jsx
@@ -1,11 +1,18 @@
-import { Divider } from 'antd';
+import { Divider, Form, Button, Input, Modal } from 'antd';
 import newsApi from 'apis/newsApi';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import NewsFieldPageSearch from './NewsFieldPageSearch/NewsFieldPageSearch';
 import NewsFieldTableData from './NewsFieldTableData/NewsFieldTableData';
 import styles from './NewsFieldPage.module.scss';
 import classNames from 'classnames/bind';
+import { Direction, NotificationType } from 'common/enum';
+import { openNotification } from 'helpers/notification';
+const { TextArea } = Input;
 
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
 const cx = classNames.bind(styles);
 
 NewsFieldPage.propTypes = {};
@@ -13,35 +20,176 @@ NewsFieldPage.propTypes = {};
 NewsFieldPage.defaultProps = {};
 
 function NewsFieldPage(props) {
-    const [newsData, setNewsData] = useState({});
+  const [newsData, setNewsData] = useState({});
+  const isFirstCall = useRef(true);
+  const [objFilter, setObjFilter] = useState({
+    currentPage: 1,
+    pageSize: 10,
+    direction: Direction.DESC,
+    orderBy: 'CreatedDate',
+    keyword: '',
+  });
 
-    useEffect(() => {
-        const fetchProductList = async () => {
-            try {
-                const params = {
-                    _page: 1,
-                    _limit: 10,
-                };
-                const response = await newsApi.getNewsFieldAll(params);
-                setNewsData(response);
-            } catch (error) {
-                console.log('Failed to fetch list: ', error);
-            }
-        };
-        fetchProductList();
-    }, []);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
 
-    return (
-        <div className={cx('wrapper')}>
-            <div className={cx('top')}>
-                <NewsFieldPageSearch />
-            </div>
-            <Divider style={{ margin: '0' }} />
-            <div className={cx('table-data')}>
-                <NewsFieldTableData data={newsData} />
-            </div>
+  /**
+   * Thay đổi bộ lọc thì gọi lại danh sách
+   */
+  useEffect(() => {
+    if (isFirstCall.current) {
+      isFirstCall.current = false;
+      return;
+    }
+    fetchProductList();
+  }, [objFilter]);
+
+  /**
+   * Gọi api lấy dữ liệu danh sách
+   */
+  const fetchProductList = async () => {
+    try {
+      const response = await newsApi.getNewsFieldAll(objFilter);
+      setNewsData({
+        data: response?.pagedData?.results ?? [],
+        total: response?.pagedData?.rowCount ?? 0,
+      });
+    } catch (error) {
+      console.log('Failed to fetch list: ', error);
+    }
+  };
+
+  /**
+   * Sử lý thay đổi text search
+   * @param {*} textSearch Từ cần tìm
+   */
+  const handleChangeTextSearch = (textSearch) => {
+    setObjFilter({ ...objFilter, keyword: textSearch });
+  };
+
+  /**
+   * Thay đổi phân trang
+   */
+  const handleChangePagination = (
+    currentPage,
+    pageSize,
+    orderBy,
+    direction
+  ) => {
+    setObjFilter({ ...objFilter, currentPage, pageSize, orderBy, direction });
+  };
+
+  const handleDeleteFieldNews = async (id) => {
+    try {
+      await newsApi.deleteFieldNews(id);
+      openNotification('Xóa nguồn tin thành công');
+      fetchProductList();
+    } catch (error) {
+      openNotification('Xóa nguồn tin thất bại', '', NotificationType.ERROR);
+    }
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    form.resetFields();
+    setIsModalOpen(false);
+  };
+
+  /**
+   * Submit form tạo nguồn tin tức
+   * @param {*} values Đối tượng submit form
+   */
+  const onFinish = (values) => {
+    values = {
+      Title: values?.title ?? '',
+      Description: values?.description ?? '',
+      Order: parseInt(values?.order ?? 0),
+      Factor: parseInt(values?.factor ?? 0),
+      BiggestFactor: parseInt(values?.biggestFactor ?? 0),
+    };
+    insertFieldNews(values);
+    form.resetFields();
+  };
+  /**
+   * Gọi api lấy dữ liệu danh sách nguồi tin tức
+   */
+  const insertFieldNews = async (values) => {
+    try {
+      await newsApi.insertFieldNews(values);
+      setIsModalOpen(false);
+      fetchProductList();
+      openNotification('Tạo mới lĩnh vực thành công');
+    } catch (error) {
+      console.log('Failed to fetch list: ', error);
+    }
+  };
+  return (
+    <div className={cx('wrapper')}>
+      {
+        //#region Modal tạo mới
+      }
+      <Modal
+        className={cx('modal-insert-source-news')}
+        title='Thêm mới lĩnh vực'
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <Form {...layout} form={form} name='control-hooks' onFinish={onFinish}>
+          <Form.Item
+            name='title'
+            label='Tiêu đề'
+            rules={[{ required: true, message: 'Tiêu đề không được để trống' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name='description' label='Mô tả'>
+            <TextArea />
+          </Form.Item>
+          <Form.Item name='order' label='Số thứ tự'>
+            <Input type='number' min={0} defaultValue={0} />
+          </Form.Item>
+          <Form.Item name='factor' label='Hệ số'>
+            <Input type='number' min={0} defaultValue={0} />
+          </Form.Item>
+          <Form.Item name='biggestFactor' label='Hệ số lớn nhất'>
+            <Input type='number' min={0} defaultValue={0} />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type='primary' htmlType='Tạo mới'>
+              Tạo mới
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+      {
+        //#endregion */
+      }
+
+      <div className={cx('top')}>
+        <NewsFieldPageSearch setTextSearch={handleChangeTextSearch} />
+        <div className={cx('btn-add-field-news')}>
+          <Button type='primary' onClick={showModal}>
+            Thêm mới
+          </Button>
         </div>
-    );
+      </div>
+
+      <Divider style={{ margin: '0' }} />
+
+      <div className={cx('table-data')}>
+        <NewsFieldTableData
+          data={newsData}
+          setPagination={handleChangePagination}
+          deleteFieldsNew={handleDeleteFieldNews}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default NewsFieldPage;
--- sources/client/news-ui/src/features/admin/pages/news/NewsFieldPage/NewsFieldTableData/NewsFieldTableData.jsx
+++ sources/client/news-ui/src/features/admin/pages/news/NewsFieldPage/NewsFieldTableData/NewsFieldTableData.jsx
@@ -1,112 +1,171 @@
-import { DeleteFilled, EditFilled } from '@ant-design/icons';
-import { Button, Space, Table, Tag } from 'antd';
+import {
+  DeleteFilled,
+  EditFilled,
+  ExclamationCircleOutlined,
+} from '@ant-design/icons';
+import { Button, Space, Table, Tag, Modal } from 'antd';
 import { commonRenderTable } from 'common/commonRender';
 import datetimeHelper from 'helpers/datetimeHelper';
 import styles from './NewsFieldTableData.module.scss';
 import classNames from 'classnames/bind';
+import { PropTypes } from 'prop-types';
+import { Direction } from 'common/enum';
 
 const cx = classNames.bind(styles);
 
-NewsFieldTableData.propTypes = {};
+NewsFieldTableData.propTypes = {
+  /**
+   * Func thay đổi phân trang
+   */
+  setPagination: PropTypes.func,
 
-NewsFieldTableData.defaultProps = {};
+  /**
+   * Xóa
+   */
+  deleteFieldsNew: PropTypes.func,
+};
+
+NewsFieldTableData.defaultProps = {
+  setPagination: () => {},
+  deleteFieldsNew: () => {},
+};
 
 function NewsFieldTableData(props) {
-    const { data } = props;
+  const { data, setPagination, deleteFieldsNew } = props;
 
-    const columns = [
-        {
-            key: 'title',
-            dataIndex: 'Title',
-            title: 'Tiêu đề',
-            render: (text) => <div>{text}</div>,
-            sorter: (a, b) => a.title - b.title,
-        },
-        {
-            key: 'OrderNumber',
-            dataIndex: 'OrderNumber',
-            title: 'Số thứ tự',
-            render: (OrderNumber) => <>{OrderNumber}</>,
-            sorter: (a, b) => a.OrderNumber - b.OrderNumber,
-            width: 100,
-            align: 'right',
-        },
-        {
-            key: 'CoefficientNumber',
-            dataIndex: 'CoefficientNumber',
-            title: 'Hệ số',
-            render: (CoefficientNumber) => <>{CoefficientNumber}</>,
-            sorter: (a, b) => a.CoefficientNumber - b.CoefficientNumber,
-            width: 100,
-            align: 'right',
-        },
-        {
-            key: 'CoefficientMaxNumber',
-            dataIndex: 'CoefficientMaxNumber',
-            title: 'Hệ số lớn nhất',
-            render: (CoefficientMaxNumber) => <>{CoefficientMaxNumber}</>,
-            sorter: (a, b) => a.CoefficientMaxNumber - b.CoefficientMaxNumber,
-            width: 130,
-            align: 'right',
-        },
-        {
-            key: 'status',
-            dataIndex: 'Status',
-            title: 'Trạng thái',
-            align: 'center',
-            width: 100,
-            sorter: (a, b) => true,
-            render: (_, { Id, Status }) => {
-                let color = Status ? 'geekblue' : 'volcano';
-                let text = Status ? 'Duyệt' : 'Hủy duyệt';
-                return (
-                    <Tag color={color} key={Id} style={{ cursor: 'pointer' }} onClick={() => handleOnClickStatus({ Id, Status })}>
-                        {text}
-                    </Tag>
-                );
-            },
-        },
-        {
-            key: 'action',
-            render: (_, record) => (
-                <Space size='middle'>
-                    <Button type='primary' icon={<EditFilled />}>
-                        Sửa
-                    </Button>
-                    <Button type='ghost' danger icon={<DeleteFilled />}>
-                        Xóa
-                    </Button>
-                </Space>
-            ),
-            width: 120,
-        },
-    ];
-
-    let dataItems = data?.data ?? [];
-    dataItems = dataItems.map((item) => {
-        var createdDate = datetimeHelper.formatDateToDateVN(item.CreatedDate);
-        return { ...item, CreatedDate: createdDate, key: item.Key };
+  function handleDeleteFieldNew(values) {
+    return Modal.confirm({
+      title: 'Xóa loại tin',
+      icon: <ExclamationCircleOutlined />,
+      content: 'Bạn có chắc chắn xóa không?',
+      okText: 'Xóa',
+      cancelText: 'Hủy',
+      onOk: () => deleteFieldNewCustom(values),
     });
+  }
 
-    function handleOnClickStatus(values) {
-        // console.log(values);
+  const deleteFieldNewCustom = (values) => {
+    if (!deleteFieldsNew) {
+      return;
     }
+    deleteFieldsNew(values.id);
+  };
 
-    return (
-        <div className={cx('wrapper')}>
-            <Table
-                columns={columns}
-                pagination={{
-                    defaultPageSize: 10,
-                    showSizeChanger: true,
-                    pageSizeOptions: [10, 20, 30],
-                    showTotal: () => commonRenderTable.showTableTotalPagination(data?.total ?? 0),
-                }}
-                dataSource={dataItems}
-                size='small'
-            />
-        </div>
+  const handleOnchangeTable = (pagination, filters, sorter, extra) => {
+    setPagination(
+      pagination.current,
+      pagination.pageSize,
+      sorter.columnKey,
+      sorter.order === 'ascend' ? Direction.ASC : Direction.DESC
     );
+  };
+
+  const columns = [
+    {
+      key: 'title',
+      dataIndex: 'title',
+      title: 'Tiêu đề',
+      render: (text) => <div>{text}</div>,
+      sorter: (a, b) => a.title - b.title,
+    },
+    {
+      key: 'order',
+      dataIndex: 'order',
+      title: 'Số thứ tự',
+      render: (order) => <>{order}</>,
+      sorter: (a, b) => a.order - b.order,
+      width: 100,
+      align: 'right',
+    },
+    {
+      key: 'factor',
+      dataIndex: 'factor',
+      title: 'Hệ số',
+      render: (factor) => <>{factor}</>,
+      sorter: (a, b) => a.factor - b.factor,
+      width: 100,
+      align: 'right',
+    },
+    {
+      key: 'biggestFactor',
+      dataIndex: 'biggestFactor',
+      title: 'Hệ số lớn nhất',
+      render: (biggestFactor) => <>{biggestFactor}</>,
+      sorter: (a, b) => a.biggestFactor - b.biggestFactor,
+      width: 130,
+      align: 'right',
+    },
+    {
+      key: 'status',
+      dataIndex: 'Status',
+      title: 'Trạng thái',
+      align: 'center',
+      width: 100,
+      sorter: (a, b) => true,
+      render: (_, { Id, Status }) => {
+        let color = Status ? 'geekblue' : 'volcano';
+        let text = Status ? 'Duyệt' : 'Hủy duyệt';
+        return (
+          <Tag
+            color={color}
+            key={Id}
+            style={{ cursor: 'pointer' }}
+            onClick={() => handleOnClickStatus({ Id, Status })}
+          >
+            {text}
+          </Tag>
+        );
+      },
+    },
+    {
+      key: 'action',
+      render: (_, record) => (
+        <Space size='middle'>
+          <Button type='primary' icon={<EditFilled />}>
+            Sửa
+          </Button>
+          <Button
+            type='ghost'
+            danger
+            icon={<DeleteFilled />}
+            onClick={() => handleDeleteFieldNew(record)}
+          >
+            Xóa
+          </Button>
+        </Space>
+      ),
+      width: 120,
+    },
+  ];
+
+  let dataItems = data?.data ?? [];
+  dataItems = dataItems.map((item) => {
+    var createdDate = datetimeHelper.formatDateToDateVN(item.CreatedDate);
+    return { ...item, CreatedDate: createdDate, key: item.id };
+  });
+
+  function handleOnClickStatus(values) {
+    // console.log(values);
+  }
+
+  return (
+    <div className={cx('wrapper')}>
+      <Table
+        columns={columns}
+        onChange={handleOnchangeTable}
+        pagination={{
+          defaultPageSize: 10,
+          showSizeChanger: true,
+          pageSizeOptions: [10, 20, 30],
+          total: data?.total,
+          showTotal: () =>
+            commonRenderTable.showTableTotalPagination(data?.total ?? 0),
+        }}
+        dataSource={dataItems}
+        size='small'
+      />
+    </div>
+  );
 }
 
 export default NewsFieldTableData;
