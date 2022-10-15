f2235458b49d16f69b3a13a9d05e317fec3b9c1b
MinhUet <buivanminh1309@gmail.com>
2022-10-15 13:24:39 +0700

    feat: hot news

--- sources/client/news-ui/src/features/admin/pages/news/NewsHotPage/NewsHotPage.jsx
+++ sources/client/news-ui/src/features/admin/pages/news/NewsHotPage/NewsHotPage.jsx
@@ -1,11 +1,15 @@
-import { Divider } from 'antd';
+import { Divider, Button, Modal } from 'antd';
 import newsApi from 'apis/newsApi';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import NewsHotPageSearch from './NewsHotPageSearch/NewsHotPageSearch';
 import NewsHotTableData from './NewsHotTableData/NewsHotTableData';
 
 import classNames from 'classnames/bind';
 import styles from './NewsHotPage.module.scss';
+import { FileAddFilled } from '@ant-design/icons';
+import { Direction, NotificationType } from 'common/enum';
+import { openNotification } from 'helpers/notification';
+import NewsHotTableDataPopUp from './NewsHotTableDataPopUp/NewsHotTableDataPopUp';
 
 const cx = classNames.bind(styles);
 
@@ -14,35 +18,144 @@ NewsHotPage.propTypes = {};
 NewsHotPage.defaultProps = {};
 
 function NewsHotPage(props) {
-    const [newsData, setNewsData] = useState({});
+  const [newsData, setNewsData] = useState({});
+  const [newsDataPopUp, setNewsDataPopUp] = useState({});
+  const isFirstCall = useRef(true);
+  const [objFilter, setObjFilter] = useState({
+    currentPage: 1,
+    pageSize: 10,
+    direction: Direction.DESC,
+    orderBy: 'CreatedDate',
+    keyword: '',
+    IsHotNews: true,
+  });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [changeRowKey, setChangeRowKey] = useState();
+  useEffect(() => {
+    if (isFirstCall.current) {
+      isFirstCall.current = false;
+      return;
+    }
+    fetchProductList();
+  }, [objFilter]);
 
-    useEffect(() => {
-        const fetchProductList = async () => {
-            try {
-                const params = {
-                    _page: 1,
-                    _limit: 10,
-                };
-                const response = await newsApi.getNewsHotAll(params);
-                setNewsData(response);
-            } catch (error) {
-                console.log('Failed to fetch list: ', error);
-            }
-        };
-        fetchProductList();
-    }, []);
+  const fetchProductList = async () => {
+    try {
+      const response = await newsApi.getNewsAll(objFilter);
+      setNewsData({
+        data: response?.PagedData?.Results ?? [],
+        total: response?.PagedData?.RowCount ?? 0,
+      });
+    } catch (error) {
+      console.log('Failed to fetch list: ', error);
+    }
+  };
 
-    return (
-        <div className={cx('wrapper')}>
-            <div className={cx('top')}>
-                <NewsHotPageSearch />
-            </div>
-            <Divider style={{ margin: '0' }} />
-            <div className={cx('table-data')}>
-                <NewsHotTableData data={newsData} />
-            </div>
+  /**
+   * Thay đổi phân trang
+   */
+  const handleChangePagination = (
+    currentPage,
+    pageSize,
+    orderBy,
+    direction
+  ) => {
+    setObjFilter({ ...objFilter, currentPage, pageSize, orderBy, direction });
+  };
+
+  const handleDeleteSourceNew = async (id) => {
+    try {
+      await newsApi.deleteSourceNew(id);
+      openNotification('Xóa nguồn tin thành công');
+      fetchProductList();
+    } catch (error) {
+      openNotification('Xóa nguồn tin thất bại', '', NotificationType.ERROR);
+    }
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+    fetchProductListPopUp();
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const fetchProductListPopUp = async () => {
+    try {
+      const response = await newsApi.getNewsAll({
+        ...objFilter,
+        IsHotNews: false,
+      });
+      setNewsDataPopUp({
+        data: response?.PagedData?.Results ?? [],
+        total: response?.PagedData?.RowCount ?? 0,
+      });
+    } catch (error) {
+      console.log('Failed to fetch list: ', error);
+    }
+  };
+
+  const handleOK = () => {
+    updateHotNews();
+  };
+
+  const updateHotNews = async () => {
+    try {
+      await newsApi.updateHotNews({
+        lstNewsPostId: changeRowKey,
+      });
+      fetchProductListPopUp();
+    } catch (error) {
+      console.log('Failed to fetch list: ', error);
+    }
+  };
+
+  const handleChangeRowKey = (value) => {
+    setChangeRowKey(value);
+  };
+  return (
+    <div className={cx('wrapper')}>
+      <Modal
+        className={cx('modal-insert-source-news')}
+        title='Thêm mới nguồn tin tức'
+        open={isModalOpen}
+        onOk={handleOK}
+        onCancel={handleCancel}
+        width={'70vw'}
+      >
+        <div className={cx('top')}>
+          <NewsHotPageSearch />
         </div>
-    );
+        <Divider style={{ marginBottom: '12px' }} />
+        <div className={cx('table-data')}>
+          <NewsHotTableDataPopUp
+            data={newsDataPopUp}
+            setPagination={handleChangePagination}
+            deleteSourceNew={handleDeleteSourceNew}
+            changeRowKey={handleChangeRowKey}
+          />
+        </div>
+      </Modal>
+
+      <div className={cx('top')}>
+        <NewsHotPageSearch />
+        <div className={cx('btn-add-source-news')}>
+          <Button type='primary' icon={<FileAddFilled />} onClick={showModal}>
+            Thêm mới
+          </Button>
+        </div>
+      </div>
+      <Divider style={{ margin: '0' }} />
+      <div className={cx('table-data')}>
+        <NewsHotTableData
+          data={newsData}
+          setPagination={handleChangePagination}
+          deleteSourceNew={handleDeleteSourceNew}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default NewsHotPage;
