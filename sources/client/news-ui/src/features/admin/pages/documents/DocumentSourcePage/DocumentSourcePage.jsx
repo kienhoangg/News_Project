7a1edc957488b56d5f3d579d7163a79984918cf0
MinhUet <buivanminh1309@gmail.com>
2022-10-16 10:31:43 +0700

    feat: Sửa bộ lọc tin tức

--- sources/client/news-ui/src/features/admin/pages/documents/DocumentSourcePage/DocumentSourcePage.jsx
+++ sources/client/news-ui/src/features/admin/pages/documents/DocumentSourcePage/DocumentSourcePage.jsx
@@ -144,10 +144,14 @@ function DocumentSourcePage(props) {
   };
 
   const renderOption = (
-    <Select placeholder='Chọn cấp cha' style={{ width: '100%' }}>
+    <Select
+      placeholder='Chọn cấp cha'
+      style={{ width: '100%' }}
+      allowClear={true}
+    >
       {newsData?.data.map((x) => (
         <Option value={x.Id} key={x.Id}>
-          {x.CategoryNewsName}
+          {x.Title}
         </Option>
       ))}
     </Select>
