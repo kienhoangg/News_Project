6e537e57696815ec3b44d64f6215cd4d4045ae1d
dvhung1 <dvhung.dev@gmail.com>
2022-10-29 23:19:33 +0700

    QuestionCategoryPage

--- sources/client/news-ui/src/features/admin/pages/questions/QuestionCategoryPage/QuestionCategoryPageSearch/QuestionCategoryPageSearch.jsx
+++ sources/client/news-ui/src/features/admin/pages/questions/QuestionCategoryPage/QuestionCategoryPageSearch/QuestionCategoryPageSearch.jsx
@@ -1,38 +1,54 @@
-import { FileAddFilled, SearchOutlined } from "@ant-design/icons";
-import { Button, Col, Input, Row } from "antd";
-import classNames from "classnames/bind";
-import styles from "./QuestionCategoryPageSearch.module.scss";
+import { FileAddFilled, SearchOutlined } from '@ant-design/icons';
+import { Button, Col, Input, Row } from 'antd';
+import classNames from 'classnames/bind';
+import styles from './QuestionCategoryPageSearch.module.scss';
+import { PropTypes } from 'prop-types';
 
 const cx = classNames.bind(styles);
 
-QuestionCategoryPageSearch.propTypes = {};
+QuestionCategoryPageSearch.propTypes = {
+    setTextSearch: PropTypes.func,
+    handleOnClickCreate: PropTypes.func,
+};
 
-QuestionCategoryPageSearch.defaultProps = {};
+QuestionCategoryPageSearch.defaultProps = {
+    setTextSearch: () => {},
+    handleOnClickCreate: () => {},
+};
 
 function QuestionCategoryPageSearch(props) {
-  return (
-    <div className={cx("wrapper")}>
-      <Row gutter={16} style={{ marginBottom: 0 }}>
-        <Col span={8}>
-          <Input style={{ width: "100%" }} placeholder="Từ khóa tìm kiếm" />
-        </Col>
-        <Col span={2}>
-          <Row justify="start">
-            <Button type="default" icon={<SearchOutlined />}>
-              Tìm kiếm
-            </Button>
-          </Row>
-        </Col>
-        <Col span={14}>
-          <Row justify="end">
-            <Button type="primary" icon={<FileAddFilled />}>
-              Tạo mới
-            </Button>
-          </Row>
-        </Col>
-      </Row>
-    </div>
-  );
+    const { setTextSearch, handleOnClickCreate } = props;
+
+    function handleChange(event) {
+        console.log(event?.target?.value);
+        if (setTextSearch) {
+            setTextSearch(event?.target?.value ?? '');
+        }
+    }
+
+    return (
+        <div className={cx('wrapper')}>
+            <Row gutter={16} style={{ marginBottom: 0 }}>
+                <Col span={8}>
+                    <Input style={{ width: '100%' }} placeholder='Từ khóa tìm kiếm' onChange={handleChange} />
+                </Col>
+                <Col span={2}>
+                    <Row justify='start'>
+                        <Button type='default' icon={<SearchOutlined />}>
+                            Tìm kiếm
+                        </Button>
+                    </Row>
+                </Col>
+                <Col span={14}>
+                    <Row justify='end'>
+                        <Button type='primary' icon={<FileAddFilled />} onClick={handleOnClickCreate}>
+                            Tạo mới
+                        </Button>
+                    </Row>
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default QuestionCategoryPageSearch;
