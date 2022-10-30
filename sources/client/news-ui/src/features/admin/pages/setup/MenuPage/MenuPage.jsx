99634149984b715ce6ddb6638e1984a8d76af55b
MinhUet <buivanminh1309@gmail.com>
2022-10-30 23:34:55 +0700

    feat: Bổ sung trim url

--- sources/client/news-ui/src/features/admin/pages/setup/MenuPage/MenuPage.jsx
+++ sources/client/news-ui/src/features/admin/pages/setup/MenuPage/MenuPage.jsx
@@ -1,5 +1,24 @@
-import { CheckOutlined, CheckSquareOutlined, CloseOutlined, EditOutlined, ExclamationCircleOutlined, FileAddFilled, InfoCircleOutlined, LineOutlined } from '@ant-design/icons';
-import { Tree, Button, Modal, Form, Select, Input, Checkbox, Row, Col } from 'antd';
+import {
+  CheckOutlined,
+  CheckSquareOutlined,
+  CloseOutlined,
+  EditOutlined,
+  ExclamationCircleOutlined,
+  FileAddFilled,
+  InfoCircleOutlined,
+  LineOutlined,
+} from '@ant-design/icons';
+import {
+  Tree,
+  Button,
+  Modal,
+  Form,
+  Select,
+  Input,
+  Checkbox,
+  Row,
+  Col,
+} from 'antd';
 import setupApi from 'apis/setupApi';
 import classNames from 'classnames/bind';
 import commonFunc from 'common/commonFunc';
@@ -57,7 +76,7 @@ function MenuPage(props) {
       dataResource.current = res?.PagedData?.Results ?? [];
       setDataTree(res?.PagedData?.Results ?? []);
     } catch (err) {
-      openNotification("Tạo mới tin thất bại", "", NotificationType.ERROR);
+      openNotification('Tạo mới tin thất bại', '', NotificationType.ERROR);
     }
   };
 
@@ -74,7 +93,7 @@ function MenuPage(props) {
         <>
           <b>Hủy duyệt</b> để có thể chỉnh sửa
         </>,
-        "",
+        '',
         NotificationType.ERROR
       );
 
@@ -97,28 +116,28 @@ function MenuPage(props) {
   }
 
   async function displayMenu(id, status) {
-    const role = commonFunc.getCookie("role");
+    const role = commonFunc.getCookie('role');
     if (role !== Role.ADMIN) {
       openNotification(
         <>
           Chỉ có <b>ADMIN</b> mới thực hiện được hành động này
         </>,
-        "",
+        '',
         NotificationType.ERROR
       );
 
       return;
     }
     Modal.confirm({
-      title: "Cập nhật trạng thái",
+      title: 'Cập nhật trạng thái',
       icon: <ExclamationCircleOutlined />,
       content: (
         <>
           Bạn có chắc chắn <b>DUYỆT/HỦY DUYỆT</b> không?
         </>
       ),
-      okText: "Cập nhật",
-      cancelText: "Hủy",
+      okText: 'Cập nhật',
+      cancelText: 'Hủy',
       onOk: async () => {
         try {
           await setupApi.updateStatusMenu({
@@ -127,9 +146,9 @@ function MenuPage(props) {
             Field: TypeUpdate.STATUS,
           });
           getMenuAll();
-          openNotification("Cập nhật thành công");
+          openNotification('Cập nhật thành công');
         } catch (error) {
-          openNotification("Cập nhật thất bại", "", NotificationType.ERROR);
+          openNotification('Cập nhật thất bại', '', NotificationType.ERROR);
         }
       },
     });
@@ -141,24 +160,24 @@ function MenuPage(props) {
         <>
           <b>Hủy duyệt</b> trước khi xóa
         </>,
-        "",
+        '',
         NotificationType.ERROR
       );
       return;
     }
     return Modal.confirm({
-      title: "Xóa menu",
+      title: 'Xóa menu',
       icon: <ExclamationCircleOutlined />,
-      content: "Bạn có chắc chắn xóa không?",
-      okText: "Xóa",
-      cancelText: "Hủy",
+      content: 'Bạn có chắc chắn xóa không?',
+      okText: 'Xóa',
+      cancelText: 'Hủy',
       onOk: async () => {
         try {
           await setupApi.deleteMenu(id);
-          openNotification("Xóa thành công");
+          openNotification('Xóa thành công');
           getMenuAll();
         } catch (error) {
-          openNotification("Xóa thất bại", "", NotificationType.ERROR);
+          openNotification('Xóa thất bại', '', NotificationType.ERROR);
         }
       },
     });
@@ -174,40 +193,40 @@ function MenuPage(props) {
     const { record } = props;
     const { Title, Id, Status } = record;
     return (
-      <div onClick={() => handleClickNode(Id)} style={{ display: "flex" }}>
+      <div onClick={() => handleClickNode(Id)} style={{ display: 'flex' }}>
         <div
           title={Title}
           style={{
-            textDecoration: Status ? "none" : "line-through",
-            fontStyle: Status ? "normal" : "italic",
+            textDecoration: Status ? 'none' : 'line-through',
+            fontStyle: Status ? 'normal' : 'italic',
           }}
         >
           {Title}
         </div>
-        <div style={{ display: displayIcon[Id] ? "block" : "none" }}>
+        <div style={{ display: displayIcon[Id] ? 'block' : 'none' }}>
           <InfoCircleOutlined
-            title="Chi tiết"
-            style={{ margin: "0 10px", padding: 4, cursor: "pointer" }}
+            title='Chi tiết'
+            style={{ margin: '0 10px', padding: 4, cursor: 'pointer' }}
             onClick={() => handleShowDetail(Id)}
           />
           <EditOutlined
-            title="Sửa menu"
-            style={{ margin: "0 10px", padding: 4, cursor: "pointer" }}
+            title='Sửa menu'
+            style={{ margin: '0 10px', padding: 4, cursor: 'pointer' }}
             onClick={() => editMenu(Id)}
           />
           <CheckSquareOutlined
-            title={Status ? "Hủy duyệt" : "Duyệt"}
+            title={Status ? 'Hủy duyệt' : 'Duyệt'}
             style={{
-              margin: "0 10px",
+              margin: '0 10px',
               padding: 4,
-              cursor: "pointer",
-              color: Status ? "#f81d22" : "#008000",
+              cursor: 'pointer',
+              color: Status ? '#f81d22' : '#008000',
             }}
             onClick={() => displayMenu(Id, Status)}
           />
           <CloseOutlined
-            title="Xóa menu"
-            style={{ margin: "0 10px", padding: 4, cursor: "pointer" }}
+            title='Xóa menu'
+            style={{ margin: '0 10px', padding: 4, cursor: 'pointer' }}
             onClick={() => deleteMenu(Id, Status)}
           />
         </div>
@@ -228,8 +247,8 @@ function MenuPage(props) {
 
   const renderOption = (
     <Select
-      placeholder="Chọn cấp cha"
-      style={{ width: "100%" }}
+      placeholder='Chọn cấp cha'
+      style={{ width: '100%' }}
       allowClear={true}
     >
       {dataResource.current
@@ -248,6 +267,7 @@ function MenuPage(props) {
 
   const onFinish = (values) => {
     values.parentId = parseInt(values?.parentId || 0);
+    values.url = (values?.url || '').trim();
     values.order = parseInt(values?.order ?? 0);
     if (mode.current === Mode.Create) {
       insertCategoryNews(values);
@@ -266,9 +286,9 @@ function MenuPage(props) {
       await setupApi.insertMenu(values);
       setIsModalOpen(false);
       getMenuAll();
-      openNotification("Tạo mới danh mục thành công");
+      openNotification('Tạo mới danh mục thành công');
     } catch (error) {
-      openNotification("Tạo mới danh mục thất bại", "", NotificationType.ERROR);
+      openNotification('Tạo mới danh mục thất bại', '', NotificationType.ERROR);
     }
   };
 
@@ -280,9 +300,9 @@ function MenuPage(props) {
       await setupApi.updateMenu(idEdit.current, values);
       setIsModalOpen(false);
       getMenuAll();
-      openNotification("Cập nhật menu thành công");
+      openNotification('Cập nhật menu thành công');
     } catch (error) {
-      openNotification("Cập nhật menu thất bại", "", NotificationType.ERROR);
+      openNotification('Cập nhật menu thất bại', '', NotificationType.ERROR);
     }
   };
 
@@ -292,13 +312,13 @@ function MenuPage(props) {
     setIsModalOpen(true);
   };
   return (
-    <div className={cx("wrapper")}>
+    <div className={cx('wrapper')}>
       {
         //#region popup thêm mới
       }
       <Modal
-        className={cx("modal-category-news")}
-        title="Thêm mới danh mục tin"
+        className={cx('modal-category-news')}
+        title='Thêm mới danh mục tin'
         open={isModalOpen}
         onCancel={handleCancel}
         footer={null}
@@ -306,7 +326,7 @@ function MenuPage(props) {
         <Form
           {...layout}
           form={form}
-          name="control-hooks"
+          name='control-hooks'
           onFinish={onFinish}
           initialValues={{
             isOpenNewTab: false,
@@ -314,19 +334,19 @@ function MenuPage(props) {
           }}
         >
           <Form.Item
-            name="title"
-            label="Tiêu đề"
-            rules={[{ required: true, message: "Tiêu đề không được để trống" }]}
+            name='title'
+            label='Tiêu đề'
+            rules={[{ required: true, message: 'Tiêu đề không được để trống' }]}
           >
             <Input />
           </Form.Item>
-          <Form.Item name="parentId" label="Danh mục cấp cha">
+          <Form.Item name='parentId' label='Danh mục cấp cha'>
             {renderOption}
           </Form.Item>
-          <Form.Item name="order" label="Số thứ tự">
-            <Input type="number" min={0} defaultValue={0} />
+          <Form.Item name='order' label='Số thứ tự'>
+            <Input type='number' min={0} defaultValue={0} />
           </Form.Item>
-          <Form.Item name="url" label="Địa chỉ (Url)">
+          <Form.Item name='url' label='Địa chỉ (Url)'>
             <Input />
           </Form.Item>
           {/*           
@@ -348,10 +368,10 @@ function MenuPage(props) {
           </Form.Item> */}
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
             <Button
-              type="primary"
-              htmlType={mode.current === Mode.Edit ? "Sửa" : "Tạo mới"}
+              type='primary'
+              htmlType={mode.current === Mode.Edit ? 'Sửa' : 'Tạo mới'}
             >
-              {mode.current === Mode.Edit ? "Sửa" : "Tạo mới"}
+              {mode.current === Mode.Edit ? 'Sửa' : 'Tạo mới'}
             </Button>
           </Form.Item>
         </Form>
@@ -360,18 +380,18 @@ function MenuPage(props) {
         //#endregion
       }
 
-      <div className={cx("top")}>
+      <div className={cx('top')}>
         <MenuSearch
           dataMenu={dataResource.current}
           changeParent={handleChangeView}
         />
-        <div className={cx("btn-add-source-news")}>
-          <Button type="primary" icon={<FileAddFilled />} onClick={showModal}>
+        <div className={cx('btn-add-source-news')}>
+          <Button type='primary' icon={<FileAddFilled />} onClick={showModal}>
             Thêm mới
           </Button>
         </div>
       </div>
-      <div className={cx("title")}>Cổng thông tin điện tử tỉnh</div>
+      <div className={cx('title')}>Cổng thông tin điện tử tỉnh</div>
       <Tree
         showLine
         defaultExpandAll={true}
@@ -383,53 +403,53 @@ function MenuPage(props) {
 
       <Modal
         open={isShowDetail}
-        title="Hiển thị thông tin"
+        title='Hiển thị thông tin'
         okButtonProps={{
           style: {
-            display: "none",
+            display: 'none',
           },
         }}
-        cancelText="Thoát"
+        cancelText='Thoát'
         onCancel={() => {
           setIsShowDetail(false);
         }}
       >
         <Row gutter={8}>
           <Col span={16}>
-            <Row gutter={16} className={cx("row-item")}>
+            <Row gutter={16} className={cx('row-item')}>
               <Col span={10}>
-                <div className={cx("row-item-label")}>Tiêu đề</div>
+                <div className={cx('row-item-label')}>Tiêu đề</div>
               </Col>
               <Col span={14}>
                 <div>{detail.current?.Title}</div>
               </Col>
             </Row>
 
-            <Row gutter={16} className={cx("row-item")}>
+            <Row gutter={16} className={cx('row-item')}>
               <Col span={10}>
-                <div className={cx("row-item-label")}>Danh mục cấp cha</div>
+                <div className={cx('row-item-label')}>Danh mục cấp cha</div>
               </Col>
               <Col span={14}>
                 <div>
                   {dataResource.current.find(
                     (x) => x.Id === detail.current?.ParentId
-                  )?.Title ?? ""}
+                  )?.Title ?? ''}
                 </div>
               </Col>
             </Row>
 
-            <Row gutter={16} className={cx("row-item")}>
+            <Row gutter={16} className={cx('row-item')}>
               <Col span={10}>
-                <div className={cx("row-item-label")}>Số thứ tự</div>
+                <div className={cx('row-item-label')}>Số thứ tự</div>
               </Col>
               <Col span={14}>
                 <div>{detail.current?.Order}</div>
               </Col>
             </Row>
 
-            <Row gutter={16} className={cx("row-item")}>
+            <Row gutter={16} className={cx('row-item')}>
               <Col span={10}>
-                <div className={cx("row-item-label")}>Địa chỉ (Url)</div>
+                <div className={cx('row-item-label')}>Địa chỉ (Url)</div>
               </Col>
               <Col span={14}>
                 <div>{detail.current?.Url}</div>
